404ee8ee251b6ec757dfbc6733881b5cb76a15a2
Brandon Perez <brandonperez@brandons-mbp.attlocal.net>
2021-08-05 16:49:28 -0400

    configs'

---
+++ configs/config_file.py
@@ -0,0 +1,15 @@
+{
+    "hyperparameters": {
+        "lr": .001,
+        "lr_steps": [50,100],
+        "epochs": 10,
+        "momentum": .9,
+        "weight_decay"=5e-4,
+        "print_freq" = 1
+
+    },
+
+    "datasets": {
+        "training_csv": "train_labels_xsmall.csv",
+    }
+}
