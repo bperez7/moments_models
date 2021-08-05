4dd95929289c1d19507d5dc6c7ecb9ba56e600a9
Brandon Perez <brandonperez@brandons-mbp.attlocal.net>
2021-08-05 16:45:33 -0400

    bash scripts

---
+++ bash_scripts/output_command.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+bash bash_scripts/crop_tool.sh -f videos/yt8m_video_library/constructions/group_7/ncUp.mp4 -l 182 -t 80 -w 853 -h 498 -o trimmed_videos/demo_vid -b 1 -e 00:00:03
+#hello
--- output_command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-bash crop_tool.sh -f videos/IMG_4884.MOV -l 594 -t 455 -w 192 -h 278 -o trimmed_videos/demo_clip -b 1 -e 00:00:09
-#hello
