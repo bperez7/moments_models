967e3e4e214d4d5f831a8cdbdb367ece8cb3fbfc
Brandon Perez <brandonperez@brandons-mbp.attlocal.net>
2021-08-10 16:48:56 -0400

    parallel

--- analysis.py
+++ analysis.py
@@ -9,6 +9,7 @@ import torch.optim
 model_folder = "trained_models"
 model_name = "model_1.h5"
 model = torch.load(model_folder+"/"+model_name)
+model = torch.nn.DataParallel(model, device_ids=[0,1]).cuda()
 
 training_true_labels = [0 for i in range(33)]
 training_true_labels[8:20] = [1 for i in range(8,20)]
