4892b40289002f22be98ff5720d92734c7bf3365
Brandon Perez <brandonperez@brandons-mbp.attlocal.net>
2021-08-23 21:07:50 -0400

    custom

--- custom_dataset.py
+++ custom_dataset.py
@@ -15,6 +15,7 @@ from torchvision.transforms import ToTensor
 import vidaug.augmentors as va
 
 import os
+import numpy as np
 import pandas as pd
 #from torchvision.io import read_image
 import models
@@ -31,13 +32,16 @@ class MachineTotalDataset(Dataset):
     def __len__(self):
         return len(self.vid_labels)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, vid_id):
         print('getting item')
-        print(idx)
+        print(vid_id)
 
 
-        vid_path = "videos/label_videos/bulldozing/bulldozing_2.mp4"
+       # vid_path = "videos/label_videos/bulldozing/bulldozing_2.mp4"
         #vid_path = os.path.join(self.vid_dir, self.vid_labels.iloc[idx, 0])
+        vid_path = os.path.join(self.vid_dir, vid_id)
+        self.vid_labels
+        label = np.array(self.vid_labels[self.vid_labels['image_id']] == vid_id['label'].values[0])
 
         #image = read_image(vid_path)
         num_segments = 8 #may need to adjust
@@ -49,7 +53,7 @@ class MachineTotalDataset(Dataset):
         #video = torch.stack([self.transform(frame) for frame in frames], 1).unsqueeze(0)
         video = torch.stack([self.transform(frame) for frame in frames], 1)
 
-        label = self.vid_labels.iloc[idx, 1]
+       # label = self.vid_labels.iloc[idx, 1]
 
         #if self.transform:
         #    image = self.transform(image)
